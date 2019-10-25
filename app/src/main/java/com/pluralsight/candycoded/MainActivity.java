25a6eb8fbf55069102331c1ed852edde433fd136
Sylvester <[start.teamhelsinki@gmail.com]>
2019-10-25 02:06:35 +0000

    completed module

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -91,8 +91,6 @@ public class MainActivity extends AppCompatActivity {
         Intent infoIntent =  new Intent(this, InfoActivity.class);
         startActivity(infoIntent);
 
-
-
         return super.onOptionsItemSelected(item);
 
 
